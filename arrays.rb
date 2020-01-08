6f66761fd6883ddb069083218c931acce7dc28a7
John <user@John-Pooleys-Macbook.local>
2020-01-08 13:00:28 +0000

    just a test

---
+++ arrays.rb
@@ -0,0 +1,31 @@
+
+
+fruits = ['apple', 'banana', 'grape', 'orange'];
+# fruits << 'lemon'
+fruits.shift()
+p fruits
+fruits.unshift('new_apple')
+# fruits.push('pear')
+# fruits[2] = 'mango'
+# # fruits[100] = 'peach'
+# fruits.pop()
+p fruits
+# # p fruits [-1]
+# p fruits [-2]
+# p fruits.first()
+# p fruits.first(2)
+# p fruits.last()
+# p fruits.last(2)
+# #
+# # p my_array
+# my_array = []
+#
+# p my_array
+my_array = Array.new()
+fruits_and_numbers = ['apple', 1, 'grape', true]
+p fruits_and_numbers
+
+what = [1, 2, 3,4,5, [6,7,8]]
+p what
+p what [5]
+p what [5] [1]
---
+++ hashes.rb
@@ -0,0 +1,38 @@
+# # meals = ['yoghurt', 'soup', 'risotto']
+# # p meals [0]
+#
+# # my_first_hash = Hash.new()
+# # my_second_hash = {}
+# #
+# # meals = {"breakfast" => "yoghurt", "lunch" => 'soup', "dinner" => 'risotto'}
+# # p meals ['lunch']
+# #
+# # meals ['supper'] = 'pancakes'
+# # meals ['dinner'] = 'pasta'
+# # p meals ['supper']
+# # meals.delete("breakfast")
+# # p meals
+# # silly_things = {1 => "2" , 2 => "3", 4 => false}
+# #
+# # p silly_things
+#
+# people = {'Lucy' => 350, 'Mike' => 20}
+# p people
+# people ['Steve'] = 90
+# p people
+# people.delete('Lucy')
+# p people
+
+
+# p :my_symbol
+# p :hello
+# 
+# meals = { :breakfast => "yoghurt",
+#           :lunch => "soup"
+#         }
+#         p meals
+
+meals = { breakfast: "yoghurt",
+          lunch: "soup"
+        }
+p meals[:breakfast]
